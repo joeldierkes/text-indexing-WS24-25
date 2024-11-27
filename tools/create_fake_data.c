e88774b7c3535bb93701eb166760cbe2ca533454
Joel Dierkes <dierkesjoel@gmail.com>
2024-11-27 12:01:45 +0100

    Comment the create_fake_data script

--- tools/create_fake_data.c
+++ tools/create_fake_data.c
@@ -1,12 +1,24 @@
-// usr/bin/env tcc -run "$0" "$@" ; exit $?
+//usr/bin/env tcc -run "$0" "$@" ; exit $?
+
+// author: Joel Dierkes <dierkesjoel@gmail.com>
+//
+// A simple tool that creates fake data for the test-indexing
+// WS2024/25 KIT module.
+//
+// Either run `./create_fake_data.c` after making the script
+// executable and having the tiny C compiler (tcc) installed or
+// compile it and run the generated executable. The output will be
+// printed to stdout and can be redirected to a file.
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
 
-#define NUMBER_INDEXES (26 + 26 + 10)
+#define ALPHABET_SIZE (26 + 26 + 10)
 
+// Branch free implementation that converts an alphabet index to the
+// representative ASCII/UTF-8 character.
 char index_to_char(size_t idx) {
   return (idx < 26) * (idx + 'a') +
          (26 <= idx && idx < 26 + 26) * (idx - 26 + 'A') +
@@ -18,7 +30,7 @@ char *generate_random_word(unsigned int n) {
 
   size_t i = 0;
   for (; i < n; ++i) {
-    ret[i] = index_to_char(rand() % NUMBER_INDEXES);
+    ret[i] = index_to_char(rand() % ALPHABET_SIZE);
   }
   ret[i] = '\0';
 
@@ -27,10 +39,13 @@ char *generate_random_word(unsigned int n) {
 
 int main(int argc, char **argv) {
   if (argc != 3) {
-    printf("Usage: ./create_fake_data.c WORD_LEN NUMBER_WORDS\n");
+    printf("Usage: ./create_fake_data.c WORD_LEN NUMBER_WORDS\n"
+	   "\n"
+	   "Example: ./create_fake_data.c 8 1000 > example_data.txt\n");
     exit(1);
   }
 
+  // It's the users task to provide valid integers.
   int n = atoi(argv[1]);
   int number_words = atoi(argv[2]);
 
